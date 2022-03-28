4ad43cebfba9f07bbcd36e1fa65e15ec0f8f8d2d
Dmitriy Muratov <muratovdima@gmail.com>
2022-03-28 22:43:46 +0300

    Updates

---
+++ precache-manifest.55f244380b1055a1c8e20e07fc17520c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "106eaa585dec33f58f13a06620b2c089",
+    "url": "/index.html"
+  },
+  {
+    "revision": "803069b6e3d4e5b752ab",
+    "url": "/static/js/2.d0bad692.chunk.js"
+  },
+  {
+    "revision": "64c0873cb99a80d912630fc251d64440",
+    "url": "/static/js/2.d0bad692.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6447aa868fe07ad815b",
+    "url": "/static/js/main.00978a96.chunk.js"
+  },
+  {
+    "revision": "9c9af111039e8883ec70",
+    "url": "/static/js/runtime-main.7daf23dc.js"
+  }
+]);
