3b19fec4c72e0e6217af5dec414441ec65e0e76f
Dmitriy Muratov <muratovdima@gmail.com>
2022-03-28 22:58:06 +0300

    build

---
+++ precache-manifest.9a2280f44a700289868dd7adf0f736e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8a00048c29bb950d8c63e4b77ec0398",
+    "url": "/space-rockets/index.html"
+  },
+  {
+    "revision": "803069b6e3d4e5b752ab",
+    "url": "/space-rockets/static/js/2.d0bad692.chunk.js"
+  },
+  {
+    "revision": "64c0873cb99a80d912630fc251d64440",
+    "url": "/space-rockets/static/js/2.d0bad692.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6447aa868fe07ad815b",
+    "url": "/space-rockets/static/js/main.00978a96.chunk.js"
+  },
+  {
+    "revision": "760bb7900628aaefd644",
+    "url": "/space-rockets/static/js/runtime-main.0559fa46.js"
+  }
+]);
